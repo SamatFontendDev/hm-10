b1d5a31ef05e6f38c45367a4fda125dffef3273d
Samatt1991 <samat116rust@yandex.ru>
2019-09-05 15:37:01 +0300

    reducers done

--- src/modules/Auth/auth.js
+++ src/modules/Auth/auth.js
@@ -1,1 +1,15 @@
 // Реализуйте редьюсер
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import { addKey } from './actions';
+
+const apiKey = handleActions({
+    [addKey]: (_state, action) => action.payload
+}, '')
+
+export const getKey = state => state.auth.apiKey
+export const getIsAuthorized = state => state.auth.apiKey ? true : false
+
+export default combineReducers({
+    apiKey
+})
--- src/modules/RoverPhotos/RoverPhotos.js
+++ src/modules/RoverPhotos/RoverPhotos.js
@@ -1,2 +1,26 @@
 // Реализуйте редьюсер
 // Файл с тестами RoverPhotos.test.js поможет вам в этом
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import { fetchPhotosRequest, fetchPhotosSuccess, fetchPhotosFailure } from './actions';
+
+const isLoading = handleActions({
+    [fetchPhotosRequest]: () => true,
+    [fetchPhotosSuccess]: () => false,
+    [fetchPhotosFailure]: () => false
+}, false)
+
+const data = handleActions({
+    [fetchPhotosRequest]: () => false,
+    [fetchPhotosSuccess]: (_state, action) => action.payload,
+}, null)
+
+const error = handleActions({
+    [fetchPhotosFailure]: (_state, action) => action.payload
+}, null)
+
+export default combineReducers({
+    isLoading,
+    data,
+    error
+})
